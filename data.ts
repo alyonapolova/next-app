90a1d30caef2391f475b0ab582eb1a1fad0e6296
alyonapolova <126259004+alyonapolova@users.noreply.github.com>
2024-06-03 16:38:25 +0200

    created next app

---
+++ data.ts
@@ -0,0 +1,41 @@
+export const songs = [
+  {
+    title: 'Bohemian Rhapsody',
+    artist: 'Queen',
+    album: 'A Night at the Opera',
+    genre: 'Rock',
+    year: 1975,
+    link: 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/9/9f/Bohemian_Rhapsody.png',
+  },
+  {
+    title: 'Smells Like Teen Spirit',
+    artist: 'Nirvana',
+    album: 'Nevermind',
+    genre: 'Grunge',
+    year: 1991,
+    link: 'https://www.youtube.com/watch?v=hTWKbfoikeg',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/b/b7/NirvanaNevermindalbumcover.jpg',
+  },
+  {
+    title: 'Billie Jean',
+    artist: 'Michael Jackson',
+    album: 'Thriller',
+    genre: 'Pop',
+    year: 1982,
+    link: 'https://www.youtube.com/watch?v=Zi_XLOBDo_Y',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/5/55/Michael_Jackson_-_Thriller.png',
+  },
+  {
+    title: 'Hotel California',
+    artist: 'Eagles',
+    album: 'Hotel California',
+    genre: 'Rock',
+    year: 1976,
+    link: 'https://www.youtube.com/watch?v=EqPtz5qN7HM',
+    image: 'https://upload.wikimedia.org/wikipedia/en/4/49/Hotelcalifornia.jpg',
+  },
+];
