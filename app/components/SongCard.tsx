27f65807d50df489584e834ecf58aaf5f7f7e048
alyonapolova <126259004+alyonapolova@users.noreply.github.com>
2024-06-03 16:51:57 +0200

    update song card

--- app/components/SongCard.tsx
+++ app/components/SongCard.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 interface ISongCardProps {
   title: string;
   artist: string;
@@ -17,7 +19,7 @@ export const SongCard = ({
 }: ISongCardProps) => {
   console.log(title);
   return (
-    <a href={link} className="group">
+    <Link href={link} className="group">
       <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <img
           src={image}
@@ -28,6 +30,6 @@ export const SongCard = ({
       <h3 className="mt-4 text-sm text-gray-700">{title}</h3>
       <p className="mt-1 text-lg font-medium text-gray-900">{artist}</p>
       <p className="mt-1 text-lg font-medium text-gray-900">{year}</p>
-    </a>
+    </Link>
   );
 };
